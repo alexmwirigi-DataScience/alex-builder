d1d3c3e6129f926ccadfa8c9c53280af43708dc6
Alex Mwirigi <mwirigialex351@gmail.com>
2026-04-05 15:42:53 +0300

    My first commit -Alex Mwirigi starts building

---
+++ hello_alex.py
@@ -0,0 +1,10 @@
+#My first real project
+#Alex Mwirigi -Builder -Nairobi, Kenya
+
+name = "Alex Mwirigi"
+city = "Nairobi"
+dream = "Mlops Engineer"
+
+print(f"My name is {name}")
+print(f"I am fom {city}")
+print("Today i stoped watching. Today I started building.")
